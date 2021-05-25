68dd43cf8457ca1da2a9fbc964efd21b514aca80
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-05-25 13:20:15 -0300

    feat: crud and relationships with MongoDB.
    updating methods of "listing", "creating" and "deleting" projects.
    
    solves task card #F0003.

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -9,15 +9,35 @@ const router = express.Router();
 router.use(authMiddleware);
 
 router.get("/", async (req, res) => {
-  res.send({ user: req.userId });
+  try {
+    const projects = await Project.find().populate("user");
+
+    return res.send({ projects });
+  } catch (err) {
+    return res.status(400).send({ error: "Error loading projects" });
+  }
 });
 
 router.get("/:projectId", async (req, res) => {
-  res.send({ user: req.userId });
+  try {
+    const project = await Project.findById(req.params.projectId).populate(
+      "user"
+    );
+
+    return res.send({ project });
+  } catch (err) {
+    return res.status(400).send({ error: "Error loading project" });
+  }
 });
 
 router.post("/", async (req, res) => {
-  res.send({ user: req.userId });
+  try {
+    const project = await Project.create({ ...req.body, user: req.userId });
+
+    return res.send({ project });
+  } catch (err) {
+    return res.status(400).send({ error: "Error creating new project" });
+  }
 });
 
 router.put("/:projectId", async (req, res) => {
@@ -25,7 +45,13 @@ router.put("/:projectId", async (req, res) => {
 });
 
 router.delete("/:projectId", async (req, res) => {
-  res.send({ user: req.userId });
+  try {
+    await Project.findByIdAndRemove(req.params.projectId);
+
+    return res.send();
+  } catch (err) {
+    return res.status(400).send({ error: "Error deleting project" });
+  }
 });
 
 module.exports = (app) => app.use("/projects", router);
